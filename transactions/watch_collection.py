8586c57d58e2f0c14bf1286d05444222ba55042f
jdrumgoole <joe@joedrumgoole.com>
2018-05-26 11:51:26 +0100

    Added python check to setup.Cleaned up code

--- transactions/watch_collection.py
+++ transactions/watch_collection.py
@@ -1,20 +1,20 @@
-
 import pymongo
 from argparse import ArgumentParser
 from datetime import datetime
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
 
     parser = ArgumentParser()
 
-    parser.add_argument( "--host", default="mongodb://localhost:27100/?replicaSet=txntest", help="mongodb URI for connecting to server [default: %(default)s]")
-    parser.add_argument( "--watch", default="test.test", help="Watch <database.colection> [default: %(default)s]")
+    parser.add_argument("--host", default="mongodb://localhost:27100/?replicaSet=txntest",
+                        help="mongodb URI for connecting to server [default: %(default)s]")
+    parser.add_argument("--watch", default="test.test", help="Watch <database.collection> [default: %(default)s]")
 
     args = parser.parse_args()
 
-    client = pymongo.MongoClient( host=args.host)
+    client = pymongo.MongoClient(host=args.host)
 
-    ( database_name, dot, collection_name ) = args.watch.partition( ".")
+    (database_name, dot, collection_name) = args.watch.partition(".")
 
     database = client[database_name]
     collection = database[collection_name]
@@ -22,17 +22,16 @@ if __name__ == "__main__" :
     watch_cursor = collection.watch()
     output = database["watcher"]
     try:
-        print( "Watching: {}\n".format(args.watch))
+        print("Watching: {}\n".format(args.watch))
         for d in watch_cursor:
             print("time now     : {}".format(datetime.utcnow()))
             print("cluster time : {}".format(d["clusterTime"].as_datetime()))
-            print("collection   : {}.{}".format(d["ns"]["db"], d["ns"]["coll"] ))
-            print("seat         : {}".format(d["fullDocument"]["seat"]) )
+            print("collection   : {}.{}".format(d["ns"]["db"], d["ns"]["coll"]))
+            print("seat         : {}".format(d["fullDocument"]["seat"]))
             print("")
-            #print(d)
+            # print(d)
             output.insert_one(d)
     except KeyboardInterrupt:
         print("Closing watch cursor")
         watch_cursor.close()
         print("exiting...")
-
